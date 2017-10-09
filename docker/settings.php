5f5e6d7ca93765d04ecc05521dd7204649db8555
giansalex <giansalex@gmail.com>
2017-10-09 11:02:05 -0500

    add box-phar build - close #2

--- docker/settings.php
+++ docker/settings.php
@@ -1,14 +1,14 @@
 <?php
 return [
     'settings' => [
-        'displayErrorDetails' => true, // set to false in production
+        'displayErrorDetails' => false, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
         // Monolog settings
         'logger' => [
             'name' => 'slim-app',
             'path' => __DIR__ . '/../logs/app.log',
-            'level' => \Monolog\Logger::DEBUG,
+            'level' => \Monolog\Logger::INFO,
         ],
 
         // Db Settings
