12d5fb8530ab9eec687b18c6d16684bb2157cc05
Giancarlos Salas <giansalex@gmail.com>
2018-03-28 12:13:49 -0500

    rename database for dev + add script build db

--- bin/travis-ci.php
+++ bin/travis-ci.php
@@ -4,7 +4,7 @@ $password = '123456';
 
 $password = password_hash($password, PASSWORD_BCRYPT);
 
-$pdo = new PDO('mysql:host=127.0.0.1;dbname=sufel', 'root', '');
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=sufel_dev', 'root', '');
 $success = $pdo->exec("INSERT INTO company(ruc, nombre, password, enable) VALUES('$ruc', 'EMPRESA SAC', '$password', 1)");
 
 if ($pdo->errorCode() != '0000') {
