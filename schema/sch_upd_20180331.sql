1e5e41c5bf68a00c12d93b44e972e10dba4d68cb
Giancarlos Salas <giansalex@gmail.com>
2018-03-31 21:38:01 -0500

    update columns client table

--- schema/sch_upd_20180331.sql
+++ schema/sch_upd_20180331.sql
@@ -1,11 +1,9 @@
 CREATE TABLE client
 (
-  tipo VARCHAR(2) NOT NULL,
   documento VARCHAR(15) NOT NULL,
+  nombres VARCHAR(100) NOT NULL,
   password VARCHAR(100) NOT NULL,
   created DATETIME DEFAULT CURRENT_TIMESTAMP,
   last_access DATETIME NOT NULL,
-  emisor CHAR(11) NOT NULL,
-  PRIMARY KEY (tipo, documento),
-  FOREIGN KEY (emisor) REFERENCES company(ruc)
+  PRIMARY KEY (documento)
 )ENGINE = INNODB;
