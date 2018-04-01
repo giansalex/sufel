bd1a0daa79ddf35082519e80b1f4c1d56b261bca
Giancarlos Salas <giansalex@gmail.com>
2018-03-31 22:58:05 -0500

    fix routes + add client actions

--- src/app/Controllers/ClientController.php
+++ src/app/Controllers/ClientController.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: Giansalex
  * Date: 28/08/2017
- * Time: 19:50
+ * Time: 19:50.
  */
 
 namespace Sufel\App\Controllers;
@@ -15,8 +15,7 @@ use Sufel\App\Repository\DocumentRepository;
 use Sufel\App\Utils\Validator;
 
 /**
- * Class ClientController
- * @package Sufel\App\Controllers
+ * Class ClientController.
  */
 class ClientController
 {
@@ -32,17 +31,23 @@ class ClientController
 
     /**
      * CompanyController constructor.
+     *
      * @param ContainerInterface $container
+     *
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
      */
-    public function __construct(ContainerInterface $container) {
+    public function __construct(ContainerInterface $container)
+    {
         $this->repository = $container->get(DocumentRepository::class);
         $this->rootDir = $container->get('settings')['upload_dir'];
     }
 
     /**
-     * @param ServerRequestInterface    $request
-     * @param Response                  $response
-     * @param array $args
+     * @param ServerRequestInterface $request
+     * @param Response               $response
+     * @param array                  $args
+     *
      * @return \Psr\Http\Message\ResponseInterface
      */
     public function getList($request, $response, $args)
@@ -60,5 +65,62 @@ class ClientController
         $docs = $this->repository->getListByClient($document, $init, $end);
 
         return $response->withJson($docs);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param Response               $response
+     * @param array                  $args
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getDocument($request, $response, $args)
+    {
+        $id = $args['id'];
+        $type = $args['type'];
+        if (!in_array($type, ['info', 'xml', 'pdf'])) {
+            return $response->withStatus(404);
+        }
+
+        $jwt = $request->getAttribute('jwt');
+        $document = $jwt->document;
+
+        $doc = $this->repository->get($id);
+        if ($doc === null) {
+            return $response->withStatus(404);
+        }
+
+        if ($doc['cliente_doc'] !== $document) {
+            return $response->withStatus(401);
+        }
+
+        if ($type == 'info') {
+            return $response->withJson($doc);
+        }
+
+        $name = $doc['filename'];
+        $pathZip = $this->rootDir.DIRECTORY_SEPARATOR.$doc['emisor'].DIRECTORY_SEPARATOR.$name.'.zip';
+        $zip = new \ZipArchive();
+        $zip->open($pathZip);
+
+        $result = [];
+        if ($type == 'xml') {
+            $result['file'] = $zip->getFromName($name.'.xml');
+            $result['type'] = 'text/xml';
+        } else {
+            $result['file'] = $zip->getFromName($name.'.pdf');
+            $result['type'] = 'application/pdf';
+        }
+        $zip->close();
+
+        $response->getBody()->write($result['file']);
+
+        return $response
+            ->withHeader('Content-Type', $result['type'])
+            ->withHeader('Content-Disposition', 'attachment')
+            ->withHeader('Content-Length', strlen($result['file']))
+            ->withoutHeader('Pragma')
+            ->withoutHeader('Expires')
+            ->withoutHeader('Cache-Control');
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -1,29 +1,37 @@
 <?php
+
 // Routes
 
 use Sufel\App\Controllers\ClientController;
 use Sufel\App\Controllers\CompanyController;
 use Sufel\App\Controllers\DocumentController;
+use Sufel\App\Controllers\ExternalFileController;
 use Sufel\App\Controllers\HomeController;
 use Sufel\App\Controllers\SecureController;
 
-$app->post('/api/companies', CompanyController::class . ':createCompany');
+$app->post('/api/companies', CompanyController::class.':createCompany');
 
 $app->group('/api/company', function () {
-    /**@var $this \Slim\App*/
-    $this->post('/auth', SecureController::class . ':company');
-    $this->get('/documents', CompanyController::class . ':getInvoices');
-    $this->post('/documents', CompanyController::class . ':addDocument');
-    $this->patch('/documents', CompanyController::class . ':anularDocument');
-    $this->post('/change-password', CompanyController::class . ':changePassword');
+    /** @var $this \Slim\App */
+    $this->post('/auth', SecureController::class.':company');
+    $this->get('/documents', CompanyController::class.':getInvoices');
+    $this->post('/documents', CompanyController::class.':addDocument');
+    $this->patch('/documents', CompanyController::class.':anularDocument');
+    $this->post('/change-password', CompanyController::class.':changePassword');
+});
+
+$app->group('/api/client', function () {
+    /** @var $this \Slim\App */
+    $this->get('/documents', ClientController::class.':getList');
+    $this->get('/documents/{id}/resource/{type}', ClientController::class.':getDocument');
 });
 
 $app->group('/api/document', function () {
-    /**@var $this \Slim\App*/
-    $this->post('/auth', SecureController::class . ':client');
-    $this->get('/resource/{type}', ClientController::class . ':getDocument');
+    /** @var $this \Slim\App */
+    $this->post('/auth', SecureController::class.':client');
+    $this->get('/resource/{type}', DocumentController::class.':getDocument');
 });
 
-$app->get('/', HomeController::class . ':home');
-$app->get('/swagger', HomeController::class . ':swagger')->setName('swagger');
-$app->get('/file/{hash}/{type}',DocumentController::class . ':download')->setName('file_download');
+$app->get('/', HomeController::class.':home');
+$app->get('/swagger', HomeController::class.':swagger')->setName('swagger');
+$app->get('/file/{hash}/{type}', ExternalFileController::class.':download')->setName('file_download');
