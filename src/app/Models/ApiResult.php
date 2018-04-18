27e4a56a7ea8f138db73d4fece5d01f204f15341
Giancarlos Salas <giansalex@gmail.com>
2018-04-18 10:50:01 -0500

    add api result model

---
+++ src/app/Models/ApiResult.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LPALQUILER-11
+ * Date: 18/04/2018
+ * Time: 10:45.
+ */
+
+namespace Sufel\App\Models;
+
+/**
+ * Class ApiResult.
+ */
+class ApiResult
+{
+    /**
+     * Success action.
+     *
+     * @var bool
+     */
+    private $success;
+    /**
+     * Http code result.
+     *
+     * @var int
+     */
+    private $statusCode;
+    /**
+     * Extra Headers in response.
+     *
+     * @var array
+     */
+    private $headers;
+    /**
+     * Data Result or message error.
+     *
+     * @var mixed
+     */
+    private $data;
+
+    /**
+     * @return bool
+     */
+    public function isSuccess()
+    {
+        return $this->success;
+    }
+
+    /**
+     * @param bool $success
+     *
+     * @return $this
+     */
+    public function setSuccess($success)
+    {
+        $this->success = $success;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * @param int $statusCode
+     *
+     * @return $this
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * @param array $headers
+     *
+     * @return $this
+     */
+    public function setHeaders($headers)
+    {
+        $this->headers = $headers;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param mixed $data
+     *
+     * @return $this
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+}
