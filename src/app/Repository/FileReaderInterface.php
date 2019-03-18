401157d591c5a95ff47dc57a953a139511aa82f8
Giancarlos Salas <giansalex@gmail.com>
2019-03-17 19:40:52 -0500

    change signature file and reader interface

--- src/app/Repository/FileReaderInterface.php
+++ src/app/Repository/FileReaderInterface.php
@@ -16,10 +16,10 @@ interface FileReaderInterface
     /**
      * Get file in specify format.
      *
-     * @param string|int $id
-     * @param string     $type Options: xml|pdf|cdr
+     * @param string $id
+     * @param string $type Options: xml,pdf,cdr,etc.
      *
      * @return string
      */
-    public function getFile($id, $type);
+    public function read($id, $type);
 }
